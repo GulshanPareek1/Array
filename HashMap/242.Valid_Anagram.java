b42b554d907780636035fd25638839e3f80a6afa
GulshanPareek1 <100085680+GulshanPareek1@users.noreply.github.com>
2023-11-05 20:21:20 +0530

    Update 242.Valid_Anagram.java

--- HashMap/242.Valid_Anagram.java
+++ HashMap/242.Valid_Anagram.java
@@ -28,3 +28,29 @@ class Solution {
         return true;
     }
 }
+
+// Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+// we're given a follow up for the question so approach for that 
+// here we can't use character array 
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        for(int i=0;i<t.length();i++){
+            char ch = t.charAt(i);
+            if(map.get(ch)!=null){
+                if(map.get(ch)==1){
+                    map.remove(ch);
+                }else{
+                    map.put(ch,map.get(ch)-1);
+                }
+            }else{
+                return false;
+            }
+        }
+        return map.isEmpty();
+    }
+}
