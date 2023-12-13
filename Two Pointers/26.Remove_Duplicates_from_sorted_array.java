ac8d6956579a3cb61301231a2b3b7b47a7414892
GulshanPareek1 <100085680+GulshanPareek1@users.noreply.github.com>
2023-12-13 23:18:29 +0530

    Create 26.Remove_Duplicates_from_sorted_array.java

---
+++ Two Pointers/26.Remove_Duplicates_from_sorted_array.java
@@ -0,0 +1,10 @@
+public int removeDuplicates(int[] nums) {
+    int i = 0;
+    for (int n : nums)
+        if (i == 0 || n > nums[i-1])
+            nums[i++] = n;
+    return i;
+}
+
+// one more solution yet to come 
+// which will be simpler in understanding 
